8c181694eb486945e299823507382316d7e33ec7
jayavarsan <jayavarsanr@gmail.com>
2024-03-07 09:52:45 +0530

    updatepage

--- backend/frontend/src/Components/Listalldata.jsx
+++ backend/frontend/src/Components/Listalldata.jsx
@@ -20,15 +20,17 @@ function Listalldata() {
             <p className='other_elemnts'>Type: {e.type}</p>
             <p className='other_elemnts'>{e.city}</p>
             <p className='other_elemnts'>Rating: {e.Rating}/5 </p>
-            <Link to={`/edit/${e._id}`} >Updae/edit</Link>
+            <div className='link_buttons'>
+            <Link className='link' to={`/edit/${e._id}`} >Updae/edit</Link>
+            <Link className='link'>Delete</Link>
+            </div>
           </div>
           <div className='map_div'>
             <iframe src={e.Location} width="100%" height="100%" frameBorder="0" allowFullScreen title={`Map ${index}`}></iframe>
           </div>
-          < Link to={`/edit/${e._id}`}></Link>  
         </div>
       ))}
-      <Link to={'/update'}>Add More Toliets</Link>
+      <Link className='link' to={'/update'}>Add More Toliets</Link>
     </>
   )
 }
